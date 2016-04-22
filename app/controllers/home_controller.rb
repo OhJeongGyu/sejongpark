035ae0168c8026c139ddf557dc28b76b29493eaf
OhJeongGyu <hhh2123623@gmail.com>
2016-04-23 06:12:44 +0900

    first

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,35 @@
+# require 'net/http'
+require "http"
+
+
+class HomeController < ApplicationController
+  def index
+  	
+  	region = ["Seoul","Chuncheon","Gangneung","Daejeon","Cheongju","Daegu","Gwangju","Jeonju","Busan","Ulleung","Jeju"]
+  	@region_json = []
+
+  	region.each do |e|
+  		# "http://api.openweathermap.org/data/2.5/weather?q="+e+"&appid=4321a224e1d0872c47a3c11a67ac40ea"
+  		json_temp = JSON.parse(HTTP.get("http://api.openweathermap.org/data/2.5/weather?q="+e+"&appid=4321a224e1d0872c47a3c11a67ac40ea"))
+
+  		temp = {
+  			"region" => e,
+  			"degree" => (json_temp["main"]["temp"]-273).round(2),
+  			"url" =>"http://openweathermap.org/img/w/"+json_temp["weather"][0]["icon"]+".png"
+  		}
+  		@region_json.push(temp)
+  		
+   		# @region_url.push(e)
+  	end
+
+ 
+	@seoul_res = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=seoul&appid=4321a224e1d0872c47a3c11a67ac40ea").to_s
+	@incheon_res = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=busan&appid=4321a224e1d0872c47a3c11a67ac40ea").to_s
+	@test = "http://openweathermap.org/img/w/"+JSON.parse(HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=busan&appid=4321a224e1d0872c47a3c11a67ac40ea"))["weather"][0]["icon"]+".png"
+	
+	# uri = URI('http://api.openweathermap.org/data/2.5/weather?q=sesoul,kr&appid=4321a224e1d0872c47a3c11a67ac40ea')
+	# uri = URI('http://api.openweathermap.org/data/2.5/weather?q=Incheon,kr&appid=4321a224e1d0872c47a3c11a67ac40ea')
+	
+  end
+
+end
