75f08087ada26861cf88d36bf1e12242e7e1f691
OhJeong Gyu <ohjeonggyu@OhJeongui-iMac.local>
2016-04-30 02:55:19 +0900

    장기 토큰으로 바꿈..

--- app/controllers/fb_controller.rb
+++ app/controllers/fb_controller.rb
@@ -9,8 +9,9 @@ class FbController < ApplicationController
 	degree = JSON.parse(HTTP.get(uri_string))["main"]["temp"]
 	# puts degree
 	uri = URI('https://graph.facebook.com/471697353029596/feed')
-	message = region +'의 온도는 ' +(degree-273).round(2).to_s+'도 입니다.'
-	res = Net::HTTP.post_form(uri, 'message' => message , 'access_token' => 'EAAQ06Kb0bSUBAEPKD7QTlWZBhHUlP5AGN6cyJpVXDX7VGu71y6qAtWwrrRZANC9ebRPTXdIgBg2qdvZC5ZC1VriJXhoxbKW21FU5S51joyarO3rCZChZCtJaPZC1UqJNAf2cIC9h7CpIxSVTTQZAHOfcHX3RkIT3wvdDa23ZBND9GZAIoIOHrThyuJ|hH749nuHwPgvgDNuF9I_Ua74ssQ')
+	message = region +"의 온도는 " +(degree-273).round(2).to_s+"도 입니다.\n" + Time.now.to_s
+
+	res = Net::HTTP.post_form(uri, 'message' => message , 'access_token' => 'aa')
 	puts res.body
 	redirect_to :back
 
